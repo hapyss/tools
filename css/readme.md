c617309942f72e0ee4e5dc8fed07352c088e90f7
yusongshi <506053687@qq.com>
2019-10-08 14:48:54 +0800

    对现有代码进行整理20191008

--- css/readme.md
+++ css/readme.md
@@ -1,1 +1,2 @@
-css通用配置：CSS.css
+﻿CSS.css-------------------css通用配置
+FontFamily.txt-----------字体中英文对照表
--- sh/查删关键词.sh
+++
@@ -1,3 +0,0 @@
-filter="搜狐微博|搜狐畅言|搜狐--|sohusid|sohuid|SOHUCS|sohu.com|sohu'|sohu\""
-kwords="搜狐|狐友|sohu"
-grep -riP "((?<=GET\s)|(?!($filter))($kwords))" 1.txt
